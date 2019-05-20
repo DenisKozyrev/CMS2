2493842613d7e50af16ce4169eedc56b589ddfb6
Denis Kozyrev <deniskozyrev55@gmail.com>
2019-05-20 18:37:56 +0300

    add our items widget

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/assets/images/`
+        path: `${__dirname}/static/assets/images/`
       }
     },
     {
